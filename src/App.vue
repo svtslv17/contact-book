6d589f3f883a9b2d36b5b2bcb8bc378085858f53
svtslv17 <svtslv17@gmail.com>
2021-08-05 23:50:32 +0300

    create components with styles

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,69 @@
-<template>Hello</template>
+<template>
+  <header>
+    <h1 class="header-title">Contacts</h1>
+  </header>
+  <contacts-form @add="addContact"></contacts-form>
+  <contacts-list :contacts="contacts"></contacts-list>
+</template>
 
-<style></style>
+<script>
+import ContactsList from "@/components/ContactsList";
+import ContactsForm from "@/components/ContactsForm";
+
+export default {
+  components: {
+    ContactsList,
+    ContactsForm,
+  },
+  data() {
+    return {
+      contacts: [
+        { id: 1, name: "Julia", lastname: "Bronson" },
+        { id: 2, name: "Mark", lastname: "Mans" },
+        { id: 3, name: "Deny", lastname: "Richt" },
+        { id: 4, name: "Zoya", lastname: "Rasmos" },
+        { id: 5, name: "Den", lastname: "Gonzales" },
+      ],
+    };
+  },
+  methods: {
+    addContact(contact) {
+      this.contacts.push(contact);
+    },
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;900&display=swap");
+* {
+  margin: 0 auto;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Roboto";
+  font-style: normal;
+}
+h1,
+h2,
+h3 {
+  font-weight: normal;
+}
+.btn,
+input {
+  padding: 5px;
+  margin-left: 15px;
+  outline: none;
+  background-color: #fff;
+  border: 1px solid rgb(126, 235, 158);
+  border-radius: 5px;
+  font-size: 16px;
+}
+
+.header-title {
+  align-items: center;
+  font-size: 25px;
+  width: 95%;
+  padding: 20px;
+  text-align: center;
+}
+</style>
---
+++ src/components/ContactsForm.vue
@@ -0,0 +1,82 @@
+<template>
+  <form class="contact-form" @submit.prevent>
+    <h2 class="form-title">Create contact</h2>
+    <div class="required-field">
+      <h3 class="field-title">Required fields</h3>
+      <div class="fields">
+        <input type="text" placeholder="Name" v-model="contact.name" />
+        <input type="text" placeholder="LastName" v-model="contact.lastname" />
+      </div>
+      <button class="btn" @click="addContact">Add</button>
+    </div>
+    <div class="custom-field">
+      <h3 class="field-title">Custom field</h3>
+      <input type="text" placeholder="Custom title" />
+      <input type="text" placeholder="Custom value" />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contact: {
+        name: "",
+        lastname: "",
+      },
+    };
+  },
+  methods: {
+    addContact() {
+      this.contact.id = Date.now();
+      this.$emit("add", this.contact);
+      this.contact = {
+        name: "",
+        lastname: "",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-title {
+  font-size: 20px;
+  padding: 20px;
+  width: 100%;
+}
+.contact-form {
+  padding: 15px;
+  border: 1px solid rgb(218, 178, 145);
+  width: 95%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 5px;
+  margin-bottom: 10px;
+}
+.custom-field {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.field-title {
+  margin-right: 10px;
+}
+.required-field,
+.custom-field {
+  display: flex;
+  margin: 10px;
+  align-items: center;
+}
+.fields {
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+}
+input {
+  margin: 5px;
+}
+</style>
